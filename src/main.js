7d62a04fd2b1287fa178c483c299bd65e859e38c
Murilo Silva Papacidero <mpapacidero3@gmail.com>
2026-08-02 11:35:58 -0300

    Open New modal and Reminders Modal complete

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 import './style.css'
+
+let openModal = document.getElementById("newBlock");
+let modal = document.getElementById("modal");
+let reminders = document.getElementById("modal-reminders")
+let buttonCancelModal = document.getElementById("buttonCancelModal");
+let buttonCancelReminders = document.getElementById("buttonCancelReminders")
+
+
+newBlock.addEventListener("click", function(){
+    modal.classList.remove("hidden");
+    modal.style.display = "grid";
+});
+
+buttonCancelModal.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+newReminder.addEventListener("click", function(){
+    reminders.classList.remove("hidden");
+    reminders.style.display = "grid";
+});
+
+buttonCancelReminders.addEventListener("click", function() {
+    reminders.style.display = "none";
+});
+
