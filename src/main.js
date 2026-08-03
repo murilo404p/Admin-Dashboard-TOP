f2083fe39bf6cfab7c51ff5de98bf49d4893698e
Murilo Silva Papacidero <mpapacidero3@gmail.com>
2026-08-03 20:52:49 -0300

    counter char add

--- src/main.js
+++ src/main.js
@@ -10,9 +10,14 @@ const buttonCancelReminders = document.getElementById("buttonCancelReminders");
 
 const inputTitle = document.getElementById("inputTitle");
 const inputText =  document.getElementById("inputText");
+const inputReminders = document.getElementById("inputReminders");
 
 const buttonSaveModal = document.getElementById("buttonSaveModal");
 const buttonSaveReminders = document.getElementById("buttonSaveReminders");
+
+const titleCharCounter =  document.getElementById("title-char-counter");
+const textCharCounter =  document.getElementById("text-char-counter");
+const remindersCharCounter = document.getElementById("reminders-char-counter");
 
 newButton.addEventListener("click", function() {
     openModal(modal);
@@ -38,6 +43,9 @@ function openModal(element) {
 function closeModal(element) {
     element.classList.remove("grid");
     element.classList.add("hidden");
+    inputText.value = "";
+    inputTitle.value = "";
+    inputReminders.value = "";
 }
 
 buttonSaveModal.addEventListener("click", function(){
@@ -46,10 +54,20 @@ buttonSaveModal.addEventListener("click", function(){
         text: inputText.value
     }   
     saveModal(project);
-})
+});
 
 function saveModal(element) {
     console.log(element);
 }
 
-  
+inputTitle.addEventListener("input", () => {
+    titleCharCounter.innerText = inputTitle.value.length;
+});
+
+inputText.addEventListener("input", ()=> {
+    textCharCounter.innerText = inputText.value.length;
+});
+
+inputReminders.addEventListener("input", () => {
+    remindersCharCounter.innerText = inputReminders.value.length;
+});
